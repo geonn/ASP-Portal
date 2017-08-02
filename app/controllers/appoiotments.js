017e11ad530ffe6a058044c7402c1c65031c1214
DESKTOP-62K2T1J\Jacky Lee <ljjjun5845@gmail.com>
2017-08-02 17:36:29 +0800

    last update scrollble view page

--- app/controllers/appoiotments.js
+++ app/controllers/appoiotments.js
@@ -1,1 +1,18 @@
 var args = arguments[0] || {};//u_id date
+
+var rotate = Ti.UI.create2DMatrix().rotate(90);
+var counterRotate = rotate.rotate(-180);
+
+var scrollView = Titanium.UI.createScrollableView({
+    views:[
+        Titanium.UI.createImageView({ image:'default_app_logo.png', transform: counterRotate }),
+        Titanium.UI.createImageView({ image:'KS_nav_ui.png', transform: counterRotate }),
+        Titanium.UI.createImageView({ image:'KS_nav_views.png', transform: counterRotate })
+    ],
+    showPagingControl:true,
+    width: 480,
+    height: 320,
+    transform: rotate
+});
+
+$.view.add(scrollView);
